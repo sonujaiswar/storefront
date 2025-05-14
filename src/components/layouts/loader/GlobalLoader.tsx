fab27ef0746988296b389a057f39586f6c5eb627
Sonu J <sonujaiswar@live.com>
2025-05-14 20:45:29 +0530

    working

--- src/components/layouts/loader/GlobalLoader.tsx
+++ src/components/layouts/loader/GlobalLoader.tsx
@@ -1,48 +1,21 @@
 "use client";
-
-import { useEffect, useState } from "react";
+import "@bprogress/core/css";
+import { BProgress } from "@bprogress/core";
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import { AnimatePresence, motion } from "framer-motion";
-import { Box, LinearProgress } from "@mui/material";
 
 export default function GlobalLoader() {
   const pathname = usePathname();
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
+    BProgress.start();
 
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 600); // Duration of loader visible after route change
+    const timer = setTimeout(() => {
+      BProgress.done();
+    }, 500); // optional delay
 
-    return () => clearTimeout(timeout);
-  }, [pathname]);
+    return () => clearTimeout(timer);
+  }, [pathname]); // 👈 triggers on route change
 
-  return (
-    <AnimatePresence>
-      {loading && (
-        <motion.div
-          key="loader"
-          initial={{ scaleX: 0 }}
-          animate={{ scaleX: 1 }}
-          exit={{ scaleX: 0 }}
-          transition={{ duration: 0.4 }}
-          style={{ transformOrigin: "left" }}
-        >
-          <Box
-            sx={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100%",
-              zIndex: 2000,
-            }}
-          >
-            <LinearProgress color="primary" />
-          </Box>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
+  return null;
 }
