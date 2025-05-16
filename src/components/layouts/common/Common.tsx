0d7a807ab0282554e6d3ff0171e68f74dac99ad8
Sonu J <sonujaiswar@live.com>
2025-05-16 16:37:01 +0530

    fixed

--- src/components/layouts/common/Common.tsx
+++ src/components/layouts/common/Common.tsx
@@ -2,20 +2,7 @@
 import * as React from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+
 import Header from "@/components/ui/navbar/header/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/types/stateTypes";
