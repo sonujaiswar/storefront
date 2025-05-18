956628fa94f9051e17876641a8b5be70d4e97949
Sonu J <sonujaiswar@live.com>
2025-05-18 18:20:09 +0530

    dashboard select fixed

--- src/components/ui/sidebar/Dashboardmenu.tsx
+++ src/components/ui/sidebar/Dashboardmenu.tsx
@@ -5,26 +5,36 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
+import React from "react";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { secureRoutes } from "@/constants/SecureRoutes";
 
 export default function Mainmenu() {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <>
-      <List>
-        {["Account", "Profile", "Orders", "Security", "Address Book"].map(
-          (text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          )
-        )}
-      </List>
-    </>
+    <List>
+      {secureRoutes.map((obj, index) => (
+        <ListItem key={index} disablePadding>
+          <ListItemButton
+            selected={selectedIndex === index}
+            onClick={(event) => handleListItemClick(event, index)}
+          >
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={obj.text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 }
---
+++ src/constants/SecureRoutes.tsx
@@ -0,0 +1,18 @@
+import { NavigationRoutes } from "./NavigationRoutes";
+
+type NavigationRouteKey = keyof typeof NavigationRoutes;
+
+const routeKeys: NavigationRouteKey[] = [
+  "dashboardPage",
+  "profilePage",
+  "orderPage",
+  "favoritePage",
+  "addressPage",
+  "securityPage",
+  "helpPage",
+];
+
+const secureRoutes = routeKeys.map((key) => NavigationRoutes[key]);
+export { secureRoutes };
+
+console.log(secureRoutes);
